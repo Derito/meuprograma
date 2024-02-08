c32be37ff09bc63c2bf7de2669be232108fe184e
Helder da Costa <helderdacsta@hotmail.com>
2024-02-08 12:01:12 +0100

    Initial commit

---
+++ src/main/java/com/helder/meuprograma/BancoAppApplication.java
@@ -0,0 +1,13 @@
+package com.helder.meuprograma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helder/meuprograma/BancoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helder.meuprograma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
